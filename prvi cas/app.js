70fb8b48f36cd317805a7b5f1c55912f629b49cc
Remzo00 <gusinacremzo00@gmail.com>
2022-12-24 22:14:52 +0100

    Tipovi podataka, promenljive...

---
+++ prvi cas/app.js
@@ -0,0 +1,26 @@
+// KOMENTAR
+//Console.log prikazuje sadrzaj u konzoli
+console.log("Hello");
+// alert("Hello");
+document.write("Hello");
+
+console.log(true, false);
+
+//PROSTI TIPOVI PODATAKA
+//1. String -> predstavlja tekst, prikazuje se navodnicima "" ''
+//2. Number -> predstavlja brojeve
+//3. Boolean -> predstavlja logicke operacije i kontrolise flow aplikacije
+//4. Undifined -> Prikazuje vrednost koja nije definisana
+//5. Null -> Null prikazuje da nesto nema vrednost
+//6. Symbol ->
+//7. Bigint ->
+
+//Promenljiva ili varijabla sluzi kao konteirner za skladistenje neke vrednosti
+
+//Pravila
+//1. Promenljiva ne moze poceti sa brojem
+//2. Ne sme postojati razmak
+var prviBroj = 10;
+var x = 5;
+var y = 5;
+console.log("Zbir je ", prviBroj + y);
