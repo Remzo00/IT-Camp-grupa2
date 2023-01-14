5b59df94751f8b5475ad879bb40a900093de4d6e
Remzo00 <gusinacremzo00@gmail.com>
2023-01-14 20:38:31 +0100

    Funkcije

---
+++ osmi cas/app.js
@@ -0,0 +1,27 @@
+//function ime_Funkcije(prvi_Parametar, drugi){
+// naredbe
+// }
+
+// ime_Funkcije(argument1, dva)
+
+//Funkcija je blok koda koja izvrsava odredjenu radnju
+//Funkcije definisemo jednom i koristimo na nivou celog programa
+//Kod postaje kraci
+
+function sabiranje(a, b) {
+  console.log(a + b);
+}
+
+const pozdrav = (ime, prezime) => {
+  console.log("Pozdrav " + ime + prezime);
+};
+
+sabiranje(5, 10);
+sabiranje(20, 30);
+pozdrav("Mensure ", "Mavric");
+pozdrav("Seide");
+
+//ES6
+
+//Arrow function
+const mnozenje = () => {};
