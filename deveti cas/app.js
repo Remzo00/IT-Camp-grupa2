89fc56d03f5ba4bed853cc1248e1259b8f01b16d
Remzo Gusinac <gusinacremzo00@gmail.com>
2023-01-21 20:10:55 +0100

    Filter i sort

--- deveti cas/app.js
+++ deveti cas/app.js
@@ -11,24 +11,63 @@ const companies = [
     { name: "Company Nine", category: "Retail", start: 1981, end: 1989 },
 ];
 
+const ages = [33, 12, 20, 65, 54, 44, 11, 13, 15, 45, 25, 18, 32];
+
+
 //forEach -> izlistava niz
 // for(let i = 0; i < companies.length; i++){
 //     console.log(companies[i])
 // }
 
-companies.forEach(function(company){
-    console.log(company)
-})
+// companies.forEach(function(company){
+//     console.log(company)
+// })
 
 //Map -> pravi novi niz na osnovu zadatog niza
 
-const companiesNames = companies.map(function(company){
-    return company.name
+// const companiesNames = companies.map(function(company){
+//     return company.name
+// })
+
+// console.log(companiesNames)
+
+//Filter -> filtrira niz i vraca trazenu vrednost
+
+// let old = []
+
+// for(let i = 0; i < ages.length; i++){
+//     if(ages[i] > 18){
+//         old.push(ages[i])
+//     }
+// }
+// console.log(old)
+
+// const old = ages.filter(function(age){
+//     if(age > 18){
+//         return true
+//     }
+// })
+// console.log(old)
+
+const retailCompany = companies.filter(function(company){
+    if(company.category === "Retail"){
+        return true
+    }
 })
 
-console.log(companiesNames)
+console.log(retailCompany)
 
-//Filter
-//Sort
+//Sort -> funckija koja sortira niz
+
+const sortCompanies = companies.sort(function(a,b){
+    if(a.end > b.start){
+        return 1
+    }else{
+        return -1
+    }
+})
+
+console.log(sortCompanies)
+
 //Reduce
 
