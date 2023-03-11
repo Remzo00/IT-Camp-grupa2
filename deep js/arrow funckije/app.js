1b1f84c7fd01439ac00d66237d20b67d30389a95
Remzo Gusinac <gusinacremzo00@gmail.com>
2023-03-11 17:58:56 +0100

    Obicne i Arrow funkcije

---
+++ deep js/arrow funckije/app.js
@@ -0,0 +1,4 @@
+// U JavaScriptu postoje dve glavne vrste funkcija - obične funkcije i arrow funkcije.
+// Obe vrste funkcija se koriste za definisanje blokova koda koji se mogu pozivati i ponovo upotrebljavati u programu,
+// ali postoje neke bitne razlike u tome kako funkcinisu
+
