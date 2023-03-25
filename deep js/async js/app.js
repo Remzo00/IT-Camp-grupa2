2bf4717d30c5e541691067cc47e12199fce6ba9a
Remzo Gusinac <gusinacremzo00@gmail.com>
2023-03-25 16:48:26 +0100

    async await

--- deep js/async js/app.js
+++ deep js/async js/app.js
@@ -30,11 +30,54 @@ promise.then((message) => {
 })
 
 //Fetching API pomocu fetch() funckije i promise
-const fetchingData = () => {
-    fetch('https://jsonplaceholder.typicode.com/posts/1')
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.log(error))
+// const fetchingData = () => {
+//     fetch('https://jsonplaceholder.typicode.com/posts/1')
+//     .then(response => response.json())
+//     .then(data => console.log(data))
+//     .catch(error => console.log(error))
+// }
+
+//funkcija koja sadrzi lazni objekat 
+//id: 1
+//ime
+//godine
+//email adresa
+ 
+const fetchingData = async () => {
+    try{
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts/1')
+        console.log(response)
+    }catch(error){
+        console.log(error)
+    }
 }
+
+fetchingData()
+
+
+
+
+
+
+const generateFakeObject = async () => {
+    const fakeObject = {
+      id: Math.floor(Math.random() * 10) + 1,
+      name: 'John Doe',
+      age: 30,
+      email: 'johndoe@example.com'
+    };
+    return fakeObject;
+  }
+
+  const displayFakeObject = async () => {
+    try {
+      const fakeObject = await generateFakeObject();
+      console.log(fakeObject);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  
+  displayFakeObject();
 
 
