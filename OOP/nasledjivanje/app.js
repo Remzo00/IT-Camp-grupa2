d5403af010a022856017f507670fd480d4ea97c8
Remzo Gusinac <gusinacremzo00@gmail.com>
2023-03-30 21:17:25 +0200

    Nasledjivanje

---
+++ OOP/nasledjivanje/app.js
@@ -0,0 +1,2 @@
+// Nasleđivanje (engl. inheritance) u JavaScript-u se odnosi na proces preuzimanja karakteristika jednog objekta od drugog objekta,
+// odnosno stvaranja hijerarhije objekata u kojoj su neki objekti izvedeni iz drugih objekata.
