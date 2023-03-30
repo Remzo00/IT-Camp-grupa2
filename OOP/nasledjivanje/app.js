58d2afbb8f9a5f016b6beb987e3cc8a23e982e44
Remzo Gusinac <gusinacremzo00@gmail.com>
2023-03-30 23:00:42 +0200

    Nasledjivanje

--- OOP/nasledjivanje/app.js
+++ OOP/nasledjivanje/app.js
@@ -3,3 +3,46 @@
 
 // extends -> kljucna rec
 // super() -> kljucna rec
+
+// Klasa Zivotinja koja ima properti vrsta i kilaza.
+// Ima tri metode: wakeUp(), eat(), sleep()
+
+//  wakeUp(){
+//     return `${this.name} is going to wake up`
+//  }
+
+class Animal{
+    constructor(breed, weight){
+        this.breed = breed
+        this.weight = weight
+    }
+
+    wakeUp(){
+       return `${this.breed} is going to wake up`
+    }
+
+    eat(){
+        return `${this.breed} is going to eat`
+    }
+
+     sleep(){
+        return `${this.breed} is going to sleep`
+    }
+}
+
+class Elephant extends Animal{
+    constructor(breed, weight){
+        super(breed, weight)
+    }
+
+    dailyRoutine(){
+        return `${super.wakeUp()} then ${super.eat()} and then ${super.sleep()}`
+    }
+}
+
+const display = (content) => {
+    console.log(content)
+}
+
+const elephant = new Elephant("African", "1T")
+display(elephant.dailyRoutine())
