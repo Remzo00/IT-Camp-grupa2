f2a40b38a7ebd8818c763cba284d7340ce335adc
Remzo Gusinac <gusinacremzo00@gmail.com>
2023-04-06 21:58:08 +0200

    enkapsulacija zadatak

--- OOP/enkapsulacija/app.js
+++ OOP/enkapsulacija/app.js
@@ -40,3 +40,67 @@ console.log(osoba.getGodine())
 //Potrebno je kreirati klase Radnik i Menadžer koje nasleđuju zajednička svojstva iz klase Osoba,
 //a zatim dodati i neke dodatne metode i svojstva.
 //Osim toga, sve privatne varijable treba da budu enkapsulirane.
+
+class Osoba {
+    constructor(ime, prezime, godine) {
+      let _ime = ime;
+      let _prezime = prezime;
+      let _godine = godine;
+      
+      this.getIme = function () {
+        return _ime;
+      };
+      
+      this.getPrezime = function () {
+        return _prezime;
+      };
+      
+      this.getGodine = function () {
+        return _godine;
+      };
+    }
+  }
+  
+  class Radnik extends Osoba {
+    constructor(ime, prezime, godine, satnica) {
+      super(ime, prezime, godine);
+      let _satnica = satnica;
+      let _radnihSati = 0;
+      
+      this.getSatnica = function () {
+        return _satnica;
+      };
+      
+      this.getRadnihSati = function () {
+        return _radnihSati;
+      };
+      
+      this.dodajSate = function (brojSati) {
+        _radnihSati += brojSati;
+      };
+      
+      this.plata = function () {
+        return _satnica * _radnihSati;
+      };
+    }
+  }
+  
+  class Menadžer extends Osoba {
+    constructor(ime, prezime, godine, nadleznosti) {
+      super(ime, prezime, godine);
+      let _nadleznosti = nadleznosti;
+      let _plata = 0;
+      
+      this.getNadleznosti = function () {
+        return _nadleznosti;
+      };
+      
+      this.getPlata = function () {
+        return _plata;
+      };
+      
+      this.postaviPlatu = function (plata) {
+        _plata = plata;
+      };
+    }
+  }
